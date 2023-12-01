026dcebc73d11c7c5c025cce43fe47367b3e7fc5
LeandroUlloque <63380623+LeandroUlloque@users.noreply.github.com>
2023-12-01 17:19:28 -0300

    creacion del componente TaskCreator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import { TaskCreator } from "./components/TaskCreator";
 
 function App() {
-  const [newTaskName, setNewTaskName] = useState();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    localStorage.setItem("task", newTaskName);
-  };
-
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter a new task"
-          onChange={(e) => setNewTaskName(e.target.value)}
-        />
-
-        <button>Save Task</button>
-      </form>
+      <TaskCreator />
     </div>
   );
 }
